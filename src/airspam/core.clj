3485d5e4199cb9eb1a044192ee4a99cec86168ef
Branan Purvine-Riley <branan@puppetlabs.com>
2013-06-25 13:52:16 -0700

    Add some simple docstrings

--- src/airspam/core.clj
+++ src/airspam/core.clj
@@ -8,6 +8,9 @@
   (:gen-class))
 
 (defn create-connection
+  "Total hack. We use a connection pool with one connection for each
+  AirPlay display so that we can guarantee the connection can be kept
+  alive as long as we want"
   []
   (pool/make-reusable-conn-manager
    {:timeout 86400
@@ -17,14 +20,17 @@
 (def displays (atom {}))
 
 (defn keys-set
+  "Get the keys from a map as a set"
   [map]
   (into #{} (keys map)))
 
 (defn uuid
+  "Generate a random UUID"
   []
   (str (java.util.UUID/randomUUID)))
 
 (defn create-display
+  "Add a new AirPlay display to our list and push an image up to it"
   [name data img]
   (let [conn (create-connection)
         disp (assoc data :conn conn)
@@ -43,36 +49,31 @@
     (swap! displays assoc name disp)))
 
 (defn remove-display
+  "Terminate a connection to an AirPlay device and remove it from the
+   list of known devices"
   [name]
   (pool/shutdown-manager (:conn (get name @displays)))
   (swap! displays dissoc name))
 
 (defn on-zc-update
-  "Prints new ZC stuff when it gets updated"
+  "ZeroConf update callback. Adds/removes AirPlay devices from the
+   list of known devices as necessary"
   [img old new]
   (let [old-keys (keys-set old)
         new-keys (keys-set new)
         added (set/difference new-keys old-keys)
         removed (set/difference old-keys new-keys)]
-    (dorun
-     (try
-       (map remove-display removed)
-       (catch Exception e
-         (pprint e))))
-    (dorun
-     (try
-       (map #(create-display % (get new %) img) added)
-       (catch Exception e
-         (pprint e))))
-    (pprint new-keys)))
+    (dorun (map remove-display removed))
+    (dorun (map #(create-display % (get new %) img) added))))
 
 (defn hang
-  "Hang the process"
-  [srv]
-  (zc/examine srv)
-  (recur srv))
+  "Loop forever so the process doesn't terminate. Obviously this isn't
+  the best way to do this. So sue me."
+  []
+  (recur))
 
 (defn load-bin
+  "Load a binary file into a byte array"
   [name]
   (with-open [input (new java.io.FileInputStream name)
               output (new java.io.ByteArrayOutputStream)]
@@ -80,14 +81,13 @@
     (.toByteArray output)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
   (when (= 1 (count args))
-    (let [img (load-bin (first args))
-          srv (zc/listen "_airplay._tcp.local."
-                         :watch #(try (on-zc-update img %1 %2)
-                                      (catch Exception e
-                                        (pprint e))))]
-      (hang srv))))
+    (let [img (load-bin (first args))]
+      (zc/listen "_airplay._tcp.local."
+                 :watch #(try (on-zc-update img %1 %2)
+                              (catch Exception e
+                                (pprint e))))
+      (hang))))
